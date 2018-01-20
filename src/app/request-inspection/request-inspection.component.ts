49c03c9d77e9a2904f9c51a1118e1f28a5615528
Braanah Craig <braanah.craig@gmail.com>
2018-01-20 16:03:56 -0500

    added receipt page[no scripts]

--- src/app/request-inspection/request-inspection.component.ts
+++ src/app/request-inspection/request-inspection.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-
 import {MatChipInputEvent} from '@angular/material';
 import {ENTER} from '@angular/cdk/keycodes';
+import {MatSnackBarModule} from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-request-inspection',
