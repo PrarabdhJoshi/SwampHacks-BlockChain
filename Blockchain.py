792b201d309741220d4fe84ec302dd1518ea3740
Eyal Abadi <eyalabadi@Eyals-MacBook-Pro.local>
2018-01-20 14:33:21 -0500

    e

--- Blockchain.py
+++ Blockchain.py
@@ -229,7 +229,7 @@ def new_transaction():
         print("this account's balance is",blockchain.balances[values['sender']])
     # Check that the required fields are in the POST'ed data
 
-    if value['merchandise'] not in blockchain.balances[values['sender']]:
+    if values['merchandise'] not in blockchain.balances[values['sender']]:
         blockchain.balances[values['sender']][values['merchandise']] == values['amount']
 
     
